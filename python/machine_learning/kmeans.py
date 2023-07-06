a9280d6c499d1908e5f209b220fe91a3e9f8a737
vibhatha <vibhatha@gmail.com>
2023-07-06 18:28:38 +0530

    fix: cleanup

--- python/machine_learning/kmeans.py
+++ python/machine_learning/kmeans.py
@@ -4,6 +4,31 @@ from abc import ABC
 from abc import abstractmethod
 
 
+def plot(X, centroids, labels, show=True, iteration=None, file_name=None):
+    # Plot the original data and clusters
+    plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis')
+    plt.scatter(centroids[:, 0], centroids[:, 1], c='red', marker='x', s=100)
+    plt.title('K-means Clustering iteration ' + str(iteration))
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    if show:
+        plt.show()
+    if iteration and file_name:
+        file_name = file_name + "_" + str(iteration)
+        plt.savefig(file_name)
+    plt.close()
+
+def comparison_plot(X, c1, l1, c2, l2):
+    # Plot the original data and clusters
+    plt.scatter(X[:, 0], X[:, 1], c=l1, cmap='viridis')
+    plt.scatter(X[:, 0], X[:, 1], c=l2, cmap='viridis')
+    plt.scatter(c1[:, 0], c1[:, 1], c='red', marker='x', s=100)
+    plt.scatter(c2[:, 0], c2[:, 1], c='orange', marker='*', s=100)
+    plt.title('K-means Clustering')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+
+
 class BaseModel(ABC):
     
     def __init__(self) -> None:
@@ -20,11 +45,12 @@ class BaseModel(ABC):
     
 class KMeans(BaseModel):
     
-    def __init__(self, n_clusters, max_iter) -> None:
+    def __init__(self, n_clusters, max_iter, file_prefix=None) -> None:
         self._n_clusters = n_clusters
         self._max_iter = max_iter
         self._centroids = None
         self._labels = None
+        self._file_prefix = file_prefix
         
     @property
     def lables(self):
@@ -49,7 +75,7 @@ class KMeans(BaseModel):
     
     def _update_centroids(self, X, n_clusters, labels):
         new_centroids = []
-        for i in range(K):
+        for i in range(n_clusters):
             new_centroids.append(np.mean(X[labels==i], axis=0)) # take average row wise (or per data point)
         return np.array(new_centroids)    
         
@@ -62,53 +88,35 @@ class KMeans(BaseModel):
             labels = self._assign_labels(distances)
         
             centroids = self._update_centroids(X, self._n_clusters, labels)
-        
+            
+            if self._file_prefix:
+                plot(X, centroids, labels, False, i, self._file_prefix)
+
         self._centroids = centroids
         self._labels = labels
     
     def predict(self, X):
         return NotImplemented("Not implemented")
-    
-
-def plot(X, centroids, labels):
-    # Plot the original data and clusters
-    plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis')
-    plt.scatter(centroids[:, 0], centroids[:, 1], c='red', marker='x', s=100)
-    plt.title('K-means Clustering')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.show()
-
-def comparison_plot(X, c1, l1, c2, l2):
-    # Plot the original data and clusters
-    plt.scatter(X[:, 0], X[:, 1], c=l1, cmap='viridis')
-    plt.scatter(X[:, 0], X[:, 1], c=l2, cmap='viridis')
-    plt.scatter(c1[:, 0], c1[:, 1], c='red', marker='x', s=100)
-    plt.scatter(c2[:, 0], c2[:, 1], c='orange', marker='*', s=100)
-    plt.title('K-means Clustering')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.show()
 
 
 
 np.random.seed(123)
 
 K = 3
-N = 1000
+N = 10000
 M = 2
 
 X = np.random.rand(N, M)
-max_iter = 1000
+max_iter = 30
 
 
-kmeans = KMeans(n_clusters=K, max_iter=300)
+kmeans = KMeans(n_clusters=K, max_iter=max_iter, file_prefix="kmeans_plots/kmeans_clustering")
 kmeans.fit(X)
 
 
 from sklearn.cluster import KMeans as SciktKMeans
 
-scikit_kmeans = SciktKMeans(init='random', n_clusters=K, random_state=123).fit(X)
+scikit_kmeans = SciktKMeans(init='random', n_clusters=K, random_state=123, max_iter=max_iter).fit(X)
 print(scikit_kmeans.cluster_centers_)
 print(kmeans.centroids)
 
